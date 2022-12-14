c86c189d0a79358207fc0af0b4355647bf498ea5
Cyprien Heusse <c.heusse@gmail.com>
2022-12-14 12:15:51 +0100

    Starting Keil implementation

--- Matlab/Trottinette.m
+++ Matlab/Trottinette.m
@@ -1,4 +1,8 @@
-ftbf
+%ftbf
+%ftbf_z
+
+clear all
+close all
 
 % Paramètres de G(s)
 Kh=24;
@@ -14,7 +18,8 @@ t3=4.84e-6;
 E=0;
 
 % Paramètre du correcteur P.I.
-ti=(K*Kc*Kh)/(2*pi*400)
+%ti=(K*Kc*Kh)/(2*pi*400)
+ti=0.0014
 %   On veut abs(Kglobal/(ti*p)) = 1 -> système simplifié
 %   p = j*w = j*2*pi*f
 
@@ -35,14 +40,32 @@ nC=nC{1};
 dC=dC{1};
 
 % Tracé du diagramme de Bode du système en Boucle Ouverte
-bode(C*G*F)
+%bode(C*G*F)
 
 % Simulation du système en boucle fermée
 Kp = 0.6;
-out = sim('ftbf',[0 0.003]);
+out = sim('ftbf',[0 0.2]);
+%figure
+%plot(out.i)
+%figure
+%plot(out.capteur)
+%figure
+%plot(out.epsilon)
+
+% Transformée bilinéaire
+Te=5e-4 % 20us
+a1=Te/(2*ti)+t1/ti;
+a0=Te/(2*ti)-t1/ti;
+I=1/ti;
+
+z = tf('z',Te);
+Cz=(a1*z+a0)/(z-1)
+[nCz,dCz]=tfdata(Cz);
+nCz=nCz{1};
+dCz=dCz{1};
+
+out = sim('ftbf_z',[0 0.05]);
 figure
 plot(out.i)
-figure
-plot(out.capteur)
 figure
 plot(out.epsilon)
